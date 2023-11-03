33ff5fd851636070ffcbd41cb232d900d574ff52
samuelahmed <samueladuval@gmail.com>
2023-11-02 23:52:50 -0700

    cleanup news feed sources

--- src/Feeds/newsFeedSources.js
+++ src/Feeds/newsFeedSources.js
@@ -4,88 +4,46 @@ const newsFeedData = [
     title: "Politico",
     url: "https://www.politico.com/rss/politicopicks.xml",
   },
-    {
-      slug: "new-york-times-homepage",
-      title: "NYT Home Page",
-      url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
-    },
-    {
-      slug: "new-york-times-world",
-      title: "NYT World",
-      url: "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
-    },
-    {
-      slug: "NYT-Business",
-      title: "NYT Business",
-      url: "https://rss.nytimes.com/services/xml/rss/nyt/Business.xml",
-    },
-    {
-      slug: "bbc-news",
-      title: "BBC News",
-      url: "https://feeds.bbci.co.uk/news/rss.xml",
-    },
-    {
-      slug: "hc-firehose",
-      title: "Hacker News Firehose",
-      url: "https://hnrss.org/newest",
-    },
-    {
-      slug: "hc-frontpage",
-      title: "Hacker News Frontpage",
-      url: "https://hnrss.org/frontpage",
-    },
-    {
-      slug: "hc-best",
-      title: "Hacker News Best",
-      url: "https://hnrss.org/best",
-    },
-    {
-      slug: "nasa",
-      title: "NASA Image of Day",
-      url: "https://www.nasa.gov/feeds/iotd-feed",
-    },
-    // {
-    //   slug: "simple-cast",
-    //   title: "Simple Cast",
-    //   url: "https://feeds.simplecast.com/BqbsxVfO",
-    // }
-  ];
+  {
+    slug: "new-york-times-homepage",
+    title: "NYT Home Page",
+    url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+  },
+  {
+    slug: "new-york-times-world",
+    title: "NYT World",
+    url: "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
+  },
+  {
+    slug: "NYT-Business",
+    title: "NYT Business",
+    url: "https://rss.nytimes.com/services/xml/rss/nyt/Business.xml",
+  },
+  {
+    slug: "bbc-news",
+    title: "BBC News",
+    url: "https://feeds.bbci.co.uk/news/rss.xml",
+  },
+  {
+    slug: "hc-firehose",
+    title: "Hacker News Firehose",
+    url: "https://hnrss.org/newest",
+  },
+  {
+    slug: "hc-frontpage",
+    title: "Hacker News Frontpage",
+    url: "https://hnrss.org/frontpage",
+  },
+  {
+    slug: "hc-best",
+    title: "Hacker News Best",
+    url: "https://hnrss.org/best",
+  },
+  {
+    slug: "nasa",
+    title: "NASA Image of Day",
+    url: "https://www.nasa.gov/feeds/iotd-feed",
+  },
+];
 
-    export default newsFeedData; 
-
-
-
-    // const url = `https://hnrss.org/frontpage?nocache=${Date.now()}`;
-// const url = `https://hn.invades.space/hn_gems_rss.xml?nocache=${Date.now()}`;
-// const url =  `https://hnrss.org/classic?nocache=${Date.now()}`; // HN Classic - only votes from acc from pre 2008
-// const url =  `https://hnrss.org/best?nocache=${Date.now()}`; // HN Best -
-// https://news.ycombinator.com/item?id=36617799 - good source of rss feeds
-
-// "https://photojournal.jpl.nasa.gov/rss/new"
-// "https://photojournal.jpl.nasa.gov/rss/targetFamily/Earth"
-// 'https://feeds.npr.org/500005/podcast.xml'
-// 'https://www.youtube.com/feeds/videos.xml?channel_id=UCz8QaiQxApLq8sLNcszYyJw'
-// 'https://feeds.npr.org/500005/podcast.xml'
-// 'https://feeds.bbci.co.uk/news/video_and_audio/world/rss.xml#'
-// "https://rss.nytimes.com/services/xml/rss/nyt/Business.xml"
-
-//     11/2/2023 2:02:10 AM - 10 hours ago
-
-// Is Adidas’ $500 Marathon Shoe Worth It?
-
-// 'https://rss.nytimes.com/services/xml/rss/nyt/World.xml'
-
-// const url = `https://hnrss.org/frontpage?nocache=${Date.now()}`;
-// const url = `https://hn.invades.space/hn_gems_rss.xml?nocache=${Date.now()}`;
-// const url =  `https://hnrss.org/classic?nocache=${Date.now()}`; // HN Classic - only votes from acc from pre 2008
-// const url =  `https://hnrss.org/best?nocache=${Date.now()}`; // HN Best -
-// https://news.ycombinator.com/item?id=36617799 - good source of rss feeds
-// "https://photojournal.jpl.nasa.gov/rss/new"
-// "https://photojournal.jpl.nasa.gov/rss/targetFamily/Earth"
-// 'https://feeds.npr.org/500005/podcast.xml'
-// 'https://www.youtube.com/feeds/videos.xml?channel_id=UCz8QaiQxApLq8sLNcszYyJw'
-// 'https://feeds.npr.org/500005/podcast.xml'
-// 'https://feeds.bbci.co.uk/news/video_and_audio/world/rss.xml#'
-// "https://rss.nytimes.com/services/xml/rss/nyt/Business.xml"
-
-// const hardCodedURL = `https://rss.nytimes.com/services/xml/rss/nyt/Business.xml?nocache=${Date.now()}`;
+export default newsFeedData;
