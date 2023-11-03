f05d0a9a7750b5597b0e4615163675b92efd13bf
samuelahmed <samueladuval@gmail.com>
2023-11-03 01:35:27 -0700

    cleanup header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,12 +1,13 @@
 export default function Header({ currentFeedInformation }: any) {
-  console.log(currentFeedInformation, "currentFeedInformation IN HEADER");
   return (
     <header className="w-full px-1 text-center text-white border-b-2 border-white">
-
-
-{currentFeedInformation && currentFeedInformation.title ? currentFeedInformation.title : 'No feed selected'}&nbsp;&nbsp;&nbsp;&nbsp;
-{currentFeedInformation && currentFeedInformation.numberOfItems ? currentFeedInformation.numberOfItems : ''} items
-
+      {currentFeedInformation && currentFeedInformation.title
+        ? currentFeedInformation.title
+        : "No feed selected"}
+      &nbsp;&nbsp;&nbsp;
+      {currentFeedInformation && currentFeedInformation.numberOfItems
+        ? currentFeedInformation.numberOfItems + " " + "items "
+        : ""}
     </header>
   );
 }
