436c53c50fdb47b9c46de7d034c0c7df43a091b2
samuelahmed <samueladuval@gmail.com>
2023-11-08 16:31:34 -0800

    reorder news sources

--- src/feedSources/newsFeedSources.js
+++ src/feedSources/newsFeedSources.js
@@ -15,39 +15,9 @@ const newsFeedData = [
     url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
   },
   {
-    slug: "new-york-times-world",
-    title: "NYT World",
-    url: "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
-  },
-  {
-    slug: "NYT-Business",
-    title: "NYT Business",
-    url: "https://rss.nytimes.com/services/xml/rss/nyt/Business.xml",
-  },
-  {
-    slug: "bbc-news",
-    title: "BBC News",
-    url: "https://feeds.bbci.co.uk/news/rss.xml",
-  },
-  {
-    slug: "hn-firehose",
-    title: "Hacker News Firehose",
-    url: "https://hnrss.org/newest",
-  },
-  {
     slug: "hn-frontpage",
     title: "Hacker News Frontpage",
     url: "https://hnrss.org/frontpage",
-  },
-  {
-    slug: "hn-best",
-    title: "Hacker News Best",
-    url: "https://hnrss.org/best",
-  },
-  {
-    slug: "hn-gems",
-    title: "Hacker News Gems",
-    url: "https://hn.invades.space/hn_gems_rss.xml",
   },
   {
     slug: "al-jazeera",
@@ -63,7 +33,37 @@ const newsFeedData = [
     slug: "dave-winer",
     title: "Scripting News",
     url: "http://scripting.com/rss.xml",
-  }
+  },
+  {
+    slug: "new-york-times-world",
+    title: "NYT World",
+    url: "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
+  },
+  {
+    slug: "NYT-Business",
+    title: "NYT Business",
+    url: "https://rss.nytimes.com/services/xml/rss/nyt/Business.xml",
+  },
+  {
+    slug: "bbc-news",
+    title: "BBC News",
+    url: "https://feeds.bbci.co.uk/news/rss.xml",
+  },
+  {
+    slug: "hn-best",
+    title: "Hacker News Best",
+    url: "https://hnrss.org/best",
+  },
+  {
+    slug: "hn-gems",
+    title: "Hacker News Gems",
+    url: "https://hn.invades.space/hn_gems_rss.xml",
+  },
+  {
+    slug: "hn-firehose",
+    title: "Hacker News Firehose",
+    url: "https://hnrss.org/newest",
+  },
 ];
 
 export default newsFeedData;
