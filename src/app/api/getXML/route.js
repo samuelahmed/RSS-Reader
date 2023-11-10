0e7fe10b46a056958be79d3336fd31bee0bb613f
samuelahmed <samueladuval@gmail.com>
2023-11-10 15:37:26 -0800

    add better error handling and validation

--- src/app/api/getXML/route.js
+++ src/app/api/getXML/route.js
@@ -5,29 +5,53 @@ function isYouTubeChannelURL(url) {
 }
 
 export async function GET(request) {
-  let url = new URL(request.url).searchParams.get("feedUrl");
+  try {
+    let url = new URL(request.url).searchParams.get("feedUrl");
 
-  if (!isYouTubeChannelURL(url)) {
-    url = `${url}?nocache=${Date.now()}`;
+    // Validate the URL
+    if (!url || url.trim() === "") {
+      return new Response(JSON.stringify({ error: "URL is empty" }), {
+        status: 400,
+      });
+    }
+
+    // Add a nocache query parameter if the URL is not a YouTube channel URL
+    if (!isYouTubeChannelURL(url)) {
+      url = `${url}?nocache=${Date.now()}`;
+    }
+
+    const response = await fetch(url);
+
+    // Check the status of the response
+    if (!response.ok) {
+      return new Response(JSON.stringify({ error: `Fetch request failed with status ${response.status}` }), {
+        status: response.status,
+      });
+    }
+
+    const xmlData = await response.text();
+    const jsonData = await xml2js.parseStringPromise(xmlData, {
+      explicitArray: false,
+      mergeAttrs: true,
+    });
+
+    // Limit the number of items to 100
+    if (jsonData.feed?.entry && jsonData.feed.entry.length > 100) {
+      jsonData.feed.entry = jsonData.feed.entry.slice(0, 100);
+    } else if (
+      jsonData.rss?.channel?.item &&
+      jsonData.rss.channel.item.length > 100
+    ) {
+      jsonData.rss.channel.item = jsonData.rss.channel.item.slice(0, 100);
+    }
+
+    return new Response(JSON.stringify(jsonData));
+  } catch (error) {
+    // Log the entire error object
+    console.error(error);
+    // Return a response with the error message
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
   }
-
-  console.log(url);
-  const response = await fetch(url);
-  const xmlData = await response.text();
-  const jsonData = await xml2js.parseStringPromise(xmlData, {
-    explicitArray: false,
-    mergeAttrs: true,
-  });
-
-  // Limit the number of items to 100
-  if (jsonData.feed?.entry && jsonData.feed.entry.length > 100) {
-    jsonData.feed.entry = jsonData.feed.entry.slice(0, 100);
-  } else if (
-    jsonData.rss?.channel?.item &&
-    jsonData.rss.channel.item.length > 100
-  ) {
-    jsonData.rss.channel.item = jsonData.rss.channel.item.slice(0, 100);
-  }
-
-  return new Response(JSON.stringify(jsonData));
 }
